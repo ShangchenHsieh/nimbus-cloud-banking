61775e134428abe5f622aa25e10193510ff6c778
Sean H <seankh4444@gmail.com>
2024-09-30 12:53:50 -0700

    Sep 30 center signup form with new looks

--- frontend_react_client/src/common_component/SignUp.js
+++ frontend_react_client/src/common_component/SignUp.js
@@ -31,30 +31,30 @@ const SignUp = () => {
     validationErrors = {};
   };
 
-  const submitRegisteration = async () => {
-    const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        first_name: formData.first_name,
-        last_name: formData.last_name,
-        phone: formData.phone,
-        username: formData.username,
-        password: formData.password,
-      }),
-    };
-    const response = await fetch('http://127.0.0.1:8000/owners', requestOptions);
-    const data = await response.json();
-    if (response.status === 409) {
-      setUsernameError(data.detail); 
-    } 
-    else {
-      // setToken(data.access_token);
-      localStorage.setItem('access_token', data.access_token);
-      navigate('/userdashboard')
-    }
-  }
-
+  // const submitRegisteration = async () => {
+  //   const requestOptions = {
+  //     method: 'POST',
+  //     headers: { 'Content-Type': 'application/json' },
+  //     body: JSON.stringify({
+  //       first_name: formData.first_name,
+  //       last_name: formData.last_name,
+  //       phone: formData.phone,
+  //       username: formData.username,
+  //       password: formData.password,
+  //     }),
+  //   };
+  //   const response = await fetch('http://127.0.0.1:8000/owners', requestOptions);
+  //   const data = await response.json();
+  //   if (response.status === 409) {
+  //     setUsernameError(data.detail); 
+  //   } 
+  //   else {
+  //     // setToken(data.access_token);
+  //     localStorage.setItem('access_token', data.access_token);
+  //     navigate('/userdashboard')
+  //   }
+  // }
+// 
   const handleSubmit = (e) => {
     e.preventDefault();
     
@@ -64,7 +64,6 @@ const SignUp = () => {
     if (!formData.username) validationErrors.username = 'Username is required';
     if (!formData.password) validationErrors.password = 'Password is required';
     if (!formData.confirm_password) validationErrors.confirm_password = 'Please confirm your password';
-
     if (formData.password !== formData.confirm_password) {
       setErrorMessage('Password and Confirm Password do not match.');
     }
@@ -75,8 +74,6 @@ const SignUp = () => {
     {
       submitRegisteration();
     }
-     
-    
   };
 
   const defaultOptions = {
@@ -91,102 +88,105 @@ const SignUp = () => {
   return (
     <>
       <Navbar />
-      <div className="About-container">
+      <div className="Signup-container">
         <div className="login">
           <div className="container">
             <div className="row">
               <div className="col-md-8 m-auto">
+                
+                <div className='signup-form-container'>
                 <div className="animation-container">
-                  <Lottie options={defaultOptions} height={300} width={300 } speed={.6} />
+                  <Lottie options={defaultOptions} height={300} width={300 } speed={.45} />
                 </div>
-                <form onSubmit={handleSubmit}>
-                  <div className="form-group">
-                    <label htmlFor="first_name" className="form-label">
-                      First Name<span className="required-asterisk">*</span>
-                    </label>
-                    <input
-                      type="text"
-                      className={`form-control form-control-lg ${errors.first_name ? 'is-invalid' : ''}`}
-                      placeholder=""
-                      name="first_name"
-                      value={formData.first_name}
-                      onChange={handleChange}
-                    />
-                    {errors.first_name && <div className="invalid-feedback">{errors.first_name}</div>}
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="last_name" className="form-label">
-                      Last Name
-                    </label>
-                    <input
-                      type="text"
-                      className="form-control form-control-lg"
-                      placeholder=""
-                      name="last_name"
-                      value={formData.last_name}
-                      onChange={handleChange}
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="phone" className="form-label">
-                      Phone
-                    </label>
-                    <input
-                      type="text"
-                      className="form-control form-control-lg"
-                      placeholder="+1(xxx)-xxx-xxxx"
-                      name="phone"
-                      value={formData.phone}
-                      onChange={handleChange}
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="username" className="form-label">
-                      Email<span className="required-asterisk">*</span>
-                    </label>
-                    <input
-                      type="email"
-                      className={`form-control form-control-lg ${errors.username ? 'is-invalid' : ''}`}
-                      placeholder="example@gmail.com"
-                      name="username"
-                      value={formData.username}
-                      onChange={handleChange}
-                    />
-                    {usernameError && <div className="invalid-feedback">{usernameError}</div>}
-                    {errors.username && <div className="invalid-feedback">{errors.username}</div>}
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="password" className="form-label">
-                      Password<span className="required-asterisk">*</span>
-                    </label>
-                    <input
-                      type="password"
-                      className={`form-control form-control-lg ${errors.password ? 'is-invalid' : ''}`}
-                      placeholder=""
-                      name="password"
-                      value={formData.password}
-                      onChange={handleChange}
-                    />
-                    {errors.password && <div className="invalid-feedback">{errors.password}</div>}
-                  </div>
+                  <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                      <label htmlFor="first_name" className="form-label">
+                        First Name<span className="required-asterisk">*</span>
+                      </label>
+                      <input
+                        type="text"
+                        className={`form-control form-control-lg ${errors.first_name ? 'is-invalid' : ''}`}
+                        placeholder=""
+                        name="first_name"
+                        value={formData.first_name}
+                        onChange={handleChange}
+                      />
+                      {errors.first_name && <div className="invalid-feedback">{errors.first_name}</div>}
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="last_name" className="form-label">
+                        Last Name
+                      </label>
+                      <input
+                        type="text"
+                        className="form-control form-control-lg"
+                        placeholder=""
+                        name="last_name"
+                        value={formData.last_name}
+                        onChange={handleChange}
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="phone" className="form-label">
+                        Phone
+                      </label>
+                      <input
+                        type="text"
+                        className="form-control form-control-lg"
+                        placeholder="+1(xxx)-xxx-xxxx"
+                        name="phone"
+                        value={formData.phone}
+                        onChange={handleChange}
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="username" className="form-label">
+                        Email<span className="required-asterisk">*</span>
+                      </label>
+                      <input
+                        type="email"
+                        className={`form-control form-control-lg ${errors.username ? 'is-invalid' : ''}`}
+                        placeholder="example@gmail.com"
+                        name="username"
+                        value={formData.username}
+                        onChange={handleChange}
+                      />
+                      {usernameError && <div className="invalid-feedback">{usernameError}</div>}
+                      {errors.username && <div className="invalid-feedback">{errors.username}</div>}
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="password" className="form-label">
+                        Password<span className="required-asterisk">*</span>
+                      </label>
+                      <input
+                        type="password"
+                        className={`form-control form-control-lg ${errors.password ? 'is-invalid' : ''}`}
+                        placeholder=""
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                      />
+                      {errors.password && <div className="invalid-feedback">{errors.password}</div>}
+                    </div>
 
-                  <div className="form-group">
-                    <label htmlFor="password" className="form-label">
-                      Confirm Password<span className="required-asterisk">*</span>
-                    </label>
-                    <input
-                      type="password"
-                      className={`form-control form-control-lg ${errors.password ? 'is-invalid' : ''}`}
-                      placeholder=""
-                      name="confirm_password"
-                      value={formData.confirm_password}
-                      onChange={handleChange}
-                    />
-                    {errors.confirm_password && <div className="invalid-feedback">{errors.confirm_password}</div>}
-                  </div>
-                  <div className="invalid-feedback">{errorMessage}</div>
-                  <input type="submit" className="signup-btn" value="Register" />
-                </form>
+                    <div className="form-group">
+                      <label htmlFor="password" className="form-label">
+                        Confirm Password<span className="required-asterisk">*</span>
+                      </label>
+                      <input
+                        type="password"
+                        className={`form-control form-control-lg ${errors.password ? 'is-invalid' : ''}`}
+                        placeholder=""
+                        name="confirm_password"
+                        value={formData.confirm_password}
+                        onChange={handleChange}
+                      />
+                      {errors.confirm_password && <div className="invalid-feedback">{errors.confirm_password}</div>}
+                    </div>
+                    <div className="invalid-feedback">{errorMessage}</div>
+                    <input type="submit" className="signup-btn" value="Register" />
+                  </form>
+                </div>
               </div>
             </div>
           </div>

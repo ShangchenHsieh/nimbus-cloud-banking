bc3878259696537c463b03bdff871cbbc6e9b827
Alx455 <alexpacheco4455@gmail.com>
2024-10-01 20:19:51 -0700

    Updated settings.py. Adding CORS policy to allow Django to recieve requests from port 3000(React frontend)

--- backend_django_client/backend_django_client/settings.py
+++ backend_django_client/backend_django_client/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Custom apps below
     'authentication',
+    'corsheaders',
 ]
 
 
@@ -62,6 +63,7 @@ SIMPLE_JWT = {
 #   -- End of custom imports --
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -69,6 +71,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",    # allow connection to React frontend on port 3000
 ]
 
 ROOT_URLCONF = 'backend_django_client.urls'

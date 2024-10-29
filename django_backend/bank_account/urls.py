9dc5aaf2d123dc2c38f98731b1d7e97ab2d3c327
sean <sean@seans-MacBook-Pro.local>
2024-10-28 18:35:48 -0700

    Oct 28 Implemented deposit & withdraw functionalities

--- django_backend/bank_account/urls.py
+++ django_backend/bank_account/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from .views import BalanceView
 from .views import AccountTypesView
+from .views import AcccountNumView
 
 urlpatterns = [
     path('balance/<str:account_type>/', BalanceView.as_view(), name='balance'),
     path('account-types/', AccountTypesView.as_view(), name='account_types'),
+    path('account-number/<str:user_id>/', AcccountNumView.as_view(), name='account_number')
 ]
--- vite_frontend/src/user_component/Deposit.jsx
+++ vite_frontend/src/user_component/Deposit.jsx
@@ -1,10 +1,59 @@
-import React, { useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import UserNavbar from "./UserNavBar";
 import './styling/Formstyle.css'
+import { jwtDecode } from "jwt-decode";
+import { UserContext } from '../user_context/UserContext';
+import { useNavigate } from 'react-router-dom';
+
 const Deposit = () => {
     const [accountNumber, setAccountNumber] = useState('');
     const [amount, setAmount] = useState('');
     const [message, setMessage] = useState('');
+    const navigate = useNavigate()
+
+
+
+
+
+    useEffect(() => {
+        const fetchAccountNumber = async () => {
+            const token = localStorage.getItem('access_token');
+            console.log("Access Token:", token); // Log the access token
+            if (!token) {
+                console.error("No access token found");
+                return;
+            }
+
+            try {
+                const decodedToken = jwtDecode(token);
+                console.log("Decoded Token:", decodedToken); // Log the decoded token
+                const id = decodedToken.user_id;
+                console.log("User ID:", id); // Log the user ID
+
+                const requestOptions = {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${token}`,
+                    },
+                };
+
+                const response = await fetch(`http://127.0.0.1:8000/account/account-number/${id}/`, requestOptions);
+                console.log("Response Status:", response.status); // Log response status
+                if (!response.ok) {
+                    throw new Error(`Error ${response.status}: ${response.statusText}`);
+                }
+                const data = await response.json();
+                console.log("Fetched Data:", data); // Log the data received
+                setAccountNumber(data.account_number[0]);
+                console.log(accountNumber)
+            } catch (error) {
+                console.error('Error fetching user data:', error);
+            }
+        };
+
+        fetchAccountNumber();
+    }, []);
 
     const handleDeposit = async (e) => {
         e.preventDefault();
@@ -14,12 +63,21 @@ const Deposit = () => {
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ account_number: accountNumber, amount: parseFloat(amount) })
             });
-            const data = await response.json();
-            setMessage(data.message || 'Deposit successful');
+            if (!response.ok) {
+                setMessage('Please enter a valid amount.');
+
+            }
+            else {
+                const data = await response.json();
+                console.log(data);
+                navigate('/userdashboard')
+            }
+
         } catch (error) {
             setMessage('Deposit failed. Please try again.');
         }
     };
+
 
     return (
         <>
@@ -31,6 +89,7 @@ const Deposit = () => {
                     placeholder="Account Number"
                     value={accountNumber}
                     onChange={(e) => setAccountNumber(e.target.value)}
+                    readOnly
                 />
                 <input
                     type="number"

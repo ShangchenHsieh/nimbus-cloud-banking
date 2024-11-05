a0ad83e55ba143e1a9d4c6964368c078ec1f4fcc
Madison Kolley <53013984+Meowitsmadi@users.noreply.github.com>
2024-11-04 18:14:09 -0800

    Dockerfiles and setup

--- django_backend/backend_django_client/settings.py
+++ django_backend/backend_django_client/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%xry$*u5js-48q*lxf)#@n9-7%od9y1=+w=tos#3oapt-_0!&i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 

7da9d77b54d529d22a040265c5b0b5a561038890
Alx455 <alexpacheco4455@gmail.com>
2024-11-18 22:03:21 -0800

    Implemented admin transactions page

--- vite_frontend/src/admin_coponent/AdminUserTransactions.jsx
+++ vite_frontend/src/admin_coponent/AdminUserTransactions.jsx
@@ -1,12 +1,75 @@
 import AdminNavbar from "./AdminNavBar";
 import "./styling/AdminUserTransactions.css";
+import AdminUserTransaction from "./AdminUserTransaction";
+import React, { useState, useEffect } from "react";
+
 
 const AdminUserTransactions = () => {
+   const [transactions, setTransactions] = useState([]);
+   const accountNumber = localStorage.getItem("currentAccountNumber");
+
+   useEffect(() => {
+      const fetchTransactions = async () => {
+         const token = localStorage.getItem("access_token");
+         if (!token) {
+            console.error("No access token found");
+            return;
+         }
+
+         const requestOptions = {
+            method: "GET",
+            headers: {
+               "Content-Type": "application/json",
+               Authorization: `Bearer ${token}`,
+            },
+         };
+
+         try {
+            const response = await fetch(
+               `http://127.0.0.1:8000/transactions/user-transactions/${accountNumber}/`,
+               requestOptions
+            );
+
+            if (!response.ok) {
+               throw new Error(`Error ${response.status}: ${response.statusText}`);
+            }
+
+            const data = await response.json();
+
+            const combinedTransactions = [
+               ...data.deposits,
+               ...data.withdrawals,
+               ...data.transfers,
+            ];
+
+            combinedTransactions.sort((a, b) => new Date(b.transaction_date) - new Date(a.transaction_date));
+
+            setTransactions(combinedTransactions);
+         } catch (error) {
+            console.error("Error fetching transactions:", error);
+         }
+      };
+      if (accountNumber) {
+         fetchTransactions();
+      }
+   }, [accountNumber]);
+
    return (
       <>
          <AdminNavbar />
          <div className="adminusertransactions-container">
             <h3 className="title">Transaction History</h3>
+            {transactions.map((transaction) => (
+               <AdminUserTransaction
+                  key={transaction.id}
+                  id={transaction.id}
+                  amount={transaction.amount}
+                  transactionType={transaction.transaction_type}
+                  transactionDate={transaction.transaction_date}
+                  provider={transaction.provider}
+                  showMoreInfo={true}
+               />
+            ))}
          </div>
       </>
    );

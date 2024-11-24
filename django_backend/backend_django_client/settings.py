178e662e4493bc2ebe4c4b77d6c6fe01be3741c5
Alx455 <alexpacheco4455@gmail.com>
2024-11-24 02:08:56 -0800

    Modified settings.py to extend access token lifetime from 10 mins to an hour and a half

--- django_backend/backend_django_client/settings.py
+++ django_backend/backend_django_client/settings.py
@@ -61,7 +61,7 @@ REST_FRAMEWORK = {
     ),
 }
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=90),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
 
